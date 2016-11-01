05b607fe8f52dab89a10ad4a52e7bca153a61f8d
aaron <aaron@aarondeMacBook-Pro.local>
2016-11-01 22:42:25 +0800

    add java_base to show example in java

---
+++ java_base/src/useclass/Student.java
@@ -0,0 +1,14 @@
+package useclass;
+/** 
+* @author kai.shang
+* @version 2016年11月1日 下午10:33:36
+*/
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is first java");
+	}
+
+	
+}
