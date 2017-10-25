8a6065168dafca45fab9ef8a27b7c671c23f4ac2
SK <skwaityou@sina.com>
2017-10-25 16:20:29 +0800

    +java enum

--- java_base/src/useclass/DataStructure.java
+++ java_base/src/useclass/DataStructure.java
@@ -8,28 +8,41 @@ import java.util.Vector;
 
 class DataStructure {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TestEnumeration();
 	}
 	
-	/**
-	 *测试枚举数据类型
-	 *Enumeration本身是个接口，不是数据结构
-	 *
-	 *
-	 *@author aaron
-	 *@Time 2016年11月4日
-	 */
+	/*
+	 * 枚举类型要用关键字enum，隐含创建数据类型Enum的子类
+	 * */
+	public static enum EnumTest{
+		MON,
+		UE,
+		WED;
+	}
+	
+	
 	static public void TestEnumeration(){
-
-		Vector<String> dayNames = new Vector<String>();
-		dayNames.add("Monday");
-		dayNames.add("Tuesday");
-		Enumeration days = dayNames.elements();
-		while(days.hasMoreElements()){
-			System.out.println(days.nextElement());
+		
+		EnumTest t1 = EnumTest.UE;
+		switch(t1){
+		case UE:
+			System.out.println("This is Monday."+t1.ordinal());
+			break;
+		default:
+			System.out.println(t1);
+			break;
 		}
+
+//		Vector<String> dayNames = new Vector<String>();
+//		dayNames.add("Monday");
+//		dayNames.add("Tuesday");
+//		Enumeration days = dayNames.elements();
+//		while(days.hasMoreElements()){
+//			System.out.println(days.nextElement());
+//		}
 	}
 
 }
